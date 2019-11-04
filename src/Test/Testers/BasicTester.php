4d8d2f7e5a274023808cf05af9b574462df2043c
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-11-04 13:17:42 +0300

    Test basic instance bound

--- src/Test/Testers/BasicTester.php
+++ src/Test/Testers/BasicTester.php
@@ -13,10 +13,35 @@ class BasicTester implements TesterContract
 
     public function test(Container $container)
     {
+        $this->testAutoResolve($container);
+        $this->testInstanceBound($container);
+    }
+
+    protected function testAutoResolve(Container $container)
+    {
         $classA = $container->make(ClassA::class);
         assert($classA instanceof ClassA, $this->errorMessage);
 
         $classB = $container->make(ClassB::class);
         assert($classB instanceof ClassB, $this->errorMessage);
+    }
+
+    protected function testInstanceBound(Container $container)
+    {
+        $classA = new ClassA();
+        $container->instance(ClassA::class, $classA);
+
+        $resolved = $container->make(ClassA::class);
+
+        assert($resolved == $classA, $this->errorMessage);
+        assert($resolved instanceof ClassA, $this->errorMessage);
+
+        $classB = new ClassB();
+        $container->instance(ClassB::class, $classB);
+
+        $resolved = $container->make(ClassB::class);
+
+        assert($resolved == $classB, $this->errorMessage);
+        assert($resolved instanceof ClassB, $this->errorMessage);
     }
 }
